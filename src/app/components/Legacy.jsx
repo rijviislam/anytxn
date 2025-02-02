becd8bd2d6ef555996aed8bb80e463f11f533788
rijviislam <rijviislam54@gmail.com>
2025-02-03 05:06:42 +0600

    revert: reverts a previous commit

--- src/app/components/Legacy.jsx
+++ src/app/components/Legacy.jsx
@@ -10,20 +10,6 @@ export default function Legacy() {
             Talk to us to find out how we can transform your organisation for
             the future
           </p>
-          {/* <button className="bg-[#FE8B53] px-10 py-3 rounded-md text-white font-semibold inter-font text-lg hover:bg-[#ff894e] transition flex items-center gap-2 shadow-2xl">
-            Contact Us
-            <svg
-              className="w-3 h-3"
-              viewBox="0 0 6 9"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M5.25 4.89844L2.0625 8.08594C1.82812 8.32031 1.47656 8.32031 1.26562 8.08594L0.726562 7.57031C0.515625 7.33594 0.515625 6.98438 0.726562 6.77344L3 4.52344L0.726562 2.25C0.515625 2.03906 0.515625 1.6875 0.726562 1.45312L1.26562 0.914062C1.47656 0.703125 1.82812 0.703125 2.0625 0.914062L5.25 4.10156C5.46094 4.33594 5.46094 4.6875 5.25 4.89844Z"
-                fill="white"
-              />
-            </svg>
-          </button> */}
 
           <button className="group bg-[#FE8B53] px-10 py-3 rounded-md text-white font-semibold inter-font text-lg hover:bg-[#ff894e] transition flex items-center gap-2 shadow-2xl">
             <span className="transition-transform duration-200 group-hover:-translate-x-1">
